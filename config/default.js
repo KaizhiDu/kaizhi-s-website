633890c6a039e09f17834cccc60bf5d2b8bfd78e
KAIZHIDU <529443744@qq.com>
2019-11-12 15:11:08 -0800

    add the mongodb to server side

---
+++ config/default.js
@@ -0,0 +1,6 @@
+module.exports = {
+  mongoURI: 'mongodb+srv://admin-kevin:2wnKt6CRkwyMbngV@emily-o9czr.mongodb.net/kaizhidu?retryWrites=true&w=majority',
+  JWTSecret: 'Thisiskaizhiwebsite',
+  githubClientId: '068383357a7087d1b2cb',
+  githubSecret: '4d060be1ecf309fe3d09c47d612997bb65483958'
+};
---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const connectDB = require('./config/db');
+
+const app = express();
+
+// connect database
+connectDB();
+
+// init middleware
+// we can use req.body
+app.use(express.json({extended: false}));
+
+app.get('/', (req, res) => res.send('API RUNNING...'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server start on port ${PORT}`));
