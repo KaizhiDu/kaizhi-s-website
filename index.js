70698f386e3871228c7cd88c588aa52e1959317b
KAIZHIDU <529443744@qq.com>
2019-11-12 16:08:57 -0800

    add the user model and route

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ app.use(express.json({extended: false}));
 
 app.get('/', (req, res) => res.send('API RUNNING...'));
 
+app.use('/api/user', require('./routes/api/user'));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server start on port ${PORT}`));
