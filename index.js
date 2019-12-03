5b630acf1bacffb1cbdead234a975a82ee96c9c1
KAIZHIDU <529443744@qq.com>
2019-12-02 20:30:45 -0800

    for second test

--- index.js
+++ index.js
@@ -33,15 +33,9 @@ app.use('/api/user', require('./routes/api/user'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/note', require('./routes/api/note'));
 
+const path = require("path");
+app.use(express.static(path.join(__dirname, "client/build")))
 
-//Serve static assets in production
-if (process.env.NODE_ENV === 'production') {
-    // Set static folder
-    app.use(express.static('client/bulid'));
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'bulid', 'index.html'));
-    });
-}
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server start on port ${PORT}`));
