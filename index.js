09e1eb238b2083dd12f3fbc47cb4e73062a7ba8b
KAIZHIDU <529443744@qq.com>
2019-12-02 21:38:33 -0800

    init

--- index.js
+++ index.js
@@ -33,8 +33,12 @@ app.use('/api/user', require('./routes/api/user'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/note', require('./routes/api/note'));
 
-const path = require("path");
-app.use(express.static(path.join(__dirname, "client/build")))
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static('client/build'));
+    app.get('*', (req, res)=>{
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    })
+}
 
 const PORT = process.env.PORT || 5000;
 
